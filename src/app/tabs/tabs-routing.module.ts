9ee695c9a5207a44c8ba7351e38071636ada390d
dcondori <dxrkio@gmail.com>
2020-05-07 13:05:48 +0200

    estructurando proyecto

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -19,18 +19,19 @@ const routes: Routes = [
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
-      {
+
+
+      
+     /* {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
       }
+      */
     ]
   },
-  {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
+//  { path: '', redirectTo: 'login', pathMatch: 'full' },
+ // { path: 'login', loadChildren: '../pages/login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
